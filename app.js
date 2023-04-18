778979d2a480426f6b12c9cf59f6ad3e2c72cdb5
Ian Mason <ianmason5@googlemail.com>
2023-04-18 16:20:43 +0100

    Done, but styline is a bit ropey

--- app.js
+++ app.js
@@ -19,4 +19,32 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', { beer: responseFromAPI[0] });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+  const beerId = req.params.id;
+  punkAPI
+    .getBeer(beerId)
+    .then(beerDetails => {
+      res.render('beer-details', { beer: beerDetails[0] });
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
